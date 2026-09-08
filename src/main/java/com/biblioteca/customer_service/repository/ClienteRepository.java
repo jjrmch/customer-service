244d974d7c00130d7c62162b48e14b0a3c417ee1
Jaime Jose Romero Molina <jarromo@hotmail.es>
2026-09-08 19:53:00 +0200

    feat: endpoint para buscar clientes por nombre o email

--- src/main/java/com/biblioteca/customer_service/repository/ClienteRepository.java
+++ src/main/java/com/biblioteca/customer_service/repository/ClienteRepository.java
@@ -3,9 +3,12 @@ package com.biblioteca.customer_service.repository;
 import com.biblioteca.customer_service.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
     Optional<Cliente> findByEmail(String email);
+
+    List<Cliente> findByNombreContainingIgnoreCaseOrEmailContainingIgnoreCase(String nombre, String email);
 }
