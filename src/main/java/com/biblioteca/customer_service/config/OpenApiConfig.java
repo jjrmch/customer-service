5e7dbfcbef1775e8f52ba662fe037048c55497d9
Jaime Jose Romero Molina <jarromo@hotmail.es>
2026-09-22 17:58:23 +0200

    feat: validacion de JWT y acceso por roles en customer-service

--- src/main/java/com/biblioteca/customer_service/config/OpenApiConfig.java
+++ src/main/java/com/biblioteca/customer_service/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.biblioteca.customer_service.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +21,12 @@ public class OpenApiConfig {
 
         return new OpenAPI()
                 .servers(List.of(server))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .info(new Info()
                         .title("Customer Service API")
                         .description("Gestión de clientes de la biblioteca")
